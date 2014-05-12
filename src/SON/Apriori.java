67b029a2e5d338028e586e7b9e25098c82dad798
andrea-jeradi <andrea.jeradi@gmail.com>
2014-05-12 09:43:49 +0200

    add binomialCoeff

--- src/SON/Apriori.java
+++ src/SON/Apriori.java
@@ -178,7 +178,43 @@ public class Apriori {
 		}
 		
 		System.out.println("Singletone: "+(frequentItem.keySet().size()-singolinonfrequenti)+" DoubleTone: "+(frequentItemset.get(k).size()-coppienonfreqquenti));
-		
+		int[][] prova = this.binomialCoeff(146, 2);
+		for(int i = 0; i < prova.length; i++){
+			for(int j = 0; j < prova[i].length; j++){
+				System.out.print(" "+prova[i][j]+"");
+			}
+			System.out.println("");
+		}
+	}
+	
+	// Returns value of Binomial Coefficient C(n, k)
+	int[][] binomialCoeff(int n, int k)
+	{
+	    int C[][] = new int[n+1][k+1];;
+	    int i, j;
+	 
+	    // Caculate value of Binomial Coefficient in bottom up manner
+	    for (i = 0; i <= n; i++)
+	    {
+	        for (j = 0; j <= min(i, k); j++)
+	        {
+	            // Base Cases
+	            if (j == 0 || j == i)
+	                C[i][j] = 1;
+	 
+	            // Calculate value using previosly stored values
+	            else
+	                C[i][j] = C[i-1][j-1] + C[i-1][j];
+	        }
+	    }
+	 
+	    return C;
+	}
+	 
+	// A utility function to return minimum of two integers
+	int min(int a, int b)
+	{
+	    return a<b?a:b;
 	}
 
 }
