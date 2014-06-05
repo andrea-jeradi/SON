d7eec242d5be1341087c39cb63e20e5e4ca4679e
francesco-donato <francesco.donato.vr@gmail.com>
2014-06-05 06:21:27 -0700

    splitted the code

--- src/SON/MRStep1.java
+++ src/SON/MRStep1.java
@@ -1,251 +1,77 @@
 package SON;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.GregorianCalendar;
-import java.util.Vector;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 public class MRStep1 extends Configured implements Tool {
 
-  private int numReducers;
-  private Path inputPath;
-  private Path outputDir;
-  
-
-  @Override
-  public int run(String[] args) throws Exception {
-	  
-	Configuration conf = this.getConf();
-			
-	//define new job instead of null using conf e setting a name
-	Job job = new Job(conf, "SON_step1");
-	//set job input format
-	job.setInputFormatClass(TextInputFormat.class);
-	//set map class and the map output key and value classes
-	job.setMapperClass(MRStep1Mapper.class);
-	job.setMapOutputKeyClass(Itemset.class);
-	job.setMapOutputValueClass(IntWritable.class);
-	//set reduce class and the reduce output key and value classes
-	job.setReducerClass(MRStep1Reducer.class);
-	job.setOutputKeyClass(Itemset.class);
-	job.setOutputValueClass(IntWritable.class);
-	//set job output format
-	job.setOutputFormatClass(TextOutputFormat.class);
-	
-	//Opzionale:set the combiner class
-	if(conf.getBoolean("useCombiner", false))
-		job.setCombinerClass(MRStep1Combiner.class);
-	
-	if(conf.getBoolean("useComparator", true))
-		job.setSortComparatorClass(ItemsetComparator.class);
-	
-	//add the input file as job input (from HDFS)
-	FileInputFormat.addInputPath(job, inputPath);
-	//set the output path for the job results (to HDFS)
-	FileOutputFormat.setOutputPath(job, outputDir);
-	//set the number of reducers. This is optional and by default is 1
-	job.setNumReduceTasks(numReducers);
-	
-	
-	//set the jar class
-	job.setJarByClass(getClass());
-	
-	//run the first job
-	job.waitForCompletion(true); //? 0 : 1; // this will execute the job
-	
-	//get the number of records and store it in a configuration variable
-	Long nRecord = job.getCounters().findCounter("org.apache.hadoop.mapred.Task$Counter", "MAP_INPUT_RECORDS").getValue();
-	//conf.setLong("basketReaded", nRecord);
-	return nRecord.intValue();
-	
-	//return res ? 0 : 1;
-  }
-
-  
-  public MRStep1(int numReducers, Path inputPath, Path outputDir){
-	  this.numReducers = numReducers; 
-	  this.inputPath = inputPath;
-	  this.outputDir = outputDir;
-  }
-  
-  
-  
-  public static void main(String args[]) throws Exception {
-	  
-	if (args.length < 4) {
-      System.out.println("Usage: SON <num_reducers> <support_threshold> <input_path> <output_path> [optionName=optionValue]* ");
-      System.exit(0);
-    }
-	
-	int numReducers = Integer.parseInt(args[0]);
-	double supportThreshold = Double.parseDouble(args[1]);
-	Path inputPath = new Path(args[2]);
-	Path outputDir = new Path(args[3]);
-	Path tmpOutputDir = new Path(outputDir.toString()+"_tmp"); 
-	
-	
-	Configuration conf = new Configuration();
-	//conf.set("mapred.map.child.java.opts", "-Xmx512m");
-	conf.setDouble("s", supportThreshold);
-	//conf.set("dfs.blocksize","67108864");
-	//conf.setInt("mapreduce.job.maps",3); 
-	conf.setLong("mapreduce.task.timeout",3600000*3);
-	
-	//gestisto gli argomenti non obbligatori
-	for(int i=4; i < args.length; i++){
-		if(args[i].toLowerCase().contains("sizeofchunk"))
-			conf.setInt("sizeOfChunk", Integer.parseInt(args[i].split("=")[1]));  
-		else if(args[i].toLowerCase().contains("usecombiner"))
-			conf.setBoolean("useCombiner", Boolean.parseBoolean(args[i].split("=")[1]));
-		else if(args[i].toLowerCase().contains("usecomparator"))
-			conf.setBoolean("useComparator", Boolean.parseBoolean(args[i].split("=")[1]));
-		
-	}
-	
-    int nRecord = ToolRunner.run(conf, new MRStep1(numReducers, inputPath, tmpOutputDir), args);
-    conf.setInt("basketReaded", nRecord);
-    
-    int res = ToolRunner.run(conf, new MRStep2(numReducers, inputPath, outputDir), args);
-    
-    
-    //elimino i risultati intermedi
-    FileSystem fs = FileSystem.get(conf); 
-    fs.delete(tmpOutputDir, true); 
-    
-    System.exit(res);
-  }
-}
-
-class MRStep1Mapper extends Mapper<LongWritable,
-									Text,
-									Itemset,
-									IntWritable> {
-
-	Vector<File> chunks;
-	int nFile;
-	int sizeOfChunk;
-	int currentSize;
-	
-	BufferedWriter bw;
-	
-	
-	@Override
-  	protected void setup(Context context) throws IOException{
-		nFile = 0;
-		currentSize = 0;
-		chunks = new Vector<File>(); 
-		sizeOfChunk = context.getConfiguration().getInt("sizeOfChunk", -1);
-		System.out.println("sizeOfChunk="+sizeOfChunk);
-		
-		chunks.add(new File("Temp"+this+"_"+nFile+".txt"));
-		bw = new BufferedWriter(new FileWriter(chunks.get(nFile)));
-
-  	}
-	
-	@Override
-  	protected void map( LongWritable key,
-						  Text value, 
-						  Context context) throws IOException, InterruptedException {
-		
-		
-		String line = value.toString();
-		
-		if(sizeOfChunk != -1  && currentSize > sizeOfChunk){
-			
-			bw.close();
-			
-			nFile++;
-			currentSize =0;
-			chunks.add(new File("Temp"+this+"_"+nFile+".txt"));
-			bw = new BufferedWriter(new FileWriter(chunks.get(nFile)));
-		}
-		
-		bw.write(line+"\n");
-		currentSize++;
-  	}
-	
-
-  
-  	@Override
-  	protected void cleanup(Context context) throws IOException, InterruptedException {
-  		bw.close();
-  		
-
-  		double s = context.getConfiguration().getDouble("s", 100);
-  				
-  		for(File f: chunks){
-  			System.out.println("nuovo file: "+f.toString());
-  			GregorianCalendar inizio = new GregorianCalendar();
-  			
-	  		MRApriori a = new MRApriori(f, s, context);
-	  		a.start();
-	  		
-	  		GregorianCalendar fine = new GregorianCalendar();
-	  		System.out.println("tempo exec: "+(fine.getTimeInMillis()-inizio.getTimeInMillis())/1000 +" sec");
-			
-			
-			f.delete();
-  		}
-		
-	}
-}
-
-class MRStep1Reducer extends Reducer<Itemset,
-									IntWritable,
-									Itemset,
-									IntWritable> {
-	
-	private IntWritable one = new IntWritable(1);
-	
+	private int numReducers;
+	private Path inputPath;
+	private Path outputDir;
 
 	@Override
-	protected void reduce(Itemset key,
-							Iterable<IntWritable> values,
-							Context context) throws IOException, InterruptedException {
-		
-		context.write(key, one);
-	
-	}
-}
+	public int run(String[] args) throws Exception {
 
+		Configuration conf = this.getConf();
 
-class MRStep1Combiner extends Reducer<Itemset,
-										IntWritable,
-										Itemset,
-										IntWritable> {
-	
-	private IntWritable one = new IntWritable(1);
-	
-	protected void setup(Context context) throws IOException{
-		System.out.println("creato combiner");
-	}
-	
-	
-	@Override
-	protected void reduce(Itemset key, 
-							Iterable<IntWritable> values, 
-							Context context) throws IOException, InterruptedException {
+		// define new job instead of null using conf e setting a name
+		Job job = new Job(conf, "SON_step1");
+		// set job input format
+		job.setInputFormatClass(TextInputFormat.class);
+		// set map class and the map output key and value classes
+		job.setMapperClass(MRStep1Mapper.class);
+		job.setMapOutputKeyClass(Itemset.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		// set reduce class and the reduce output key and value classes
+		job.setReducerClass(MRStep1Reducer.class);
+		job.setOutputKeyClass(Itemset.class);
+		job.setOutputValueClass(IntWritable.class);
+		// set job output format
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-		context.write(key, one);
-	
+		// Opzionale:set the combiner class
+		if (conf.getBoolean("useCombiner", false))
+			job.setCombinerClass(MRStep1Combiner.class);
+
+		if (conf.getBoolean("useComparator", true))
+			job.setSortComparatorClass(ItemsetComparator.class);
+
+		// add the input file as job input (from HDFS)
+		FileInputFormat.addInputPath(job, inputPath);
+		// set the output path for the job results (to HDFS)
+		FileOutputFormat.setOutputPath(job, outputDir);
+		// set the number of reducers. This is optional and by default is 1
+		job.setNumReduceTasks(numReducers);
+
+		// set the jar class
+		job.setJarByClass(getClass());
+
+		// run the first job
+		job.waitForCompletion(true); // ? 0 : 1; // this will execute the job
+
+		// get the number of records and store it in a configuration variable
+		Long nRecord = job
+				.getCounters()
+				.findCounter("org.apache.hadoop.mapred.Task$Counter",
+						"MAP_INPUT_RECORDS").getValue();
+		// conf.setLong("basketReaded", nRecord);
+		return nRecord.intValue();
+
+		// return res ? 0 : 1;
 	}
+
+	public MRStep1(int numReducers, Path inputPath, Path outputDir) {
+		this.numReducers = numReducers;
+		this.inputPath = inputPath;
+		this.outputDir = outputDir;
+	}
+
 }
--- src/SON/MRStep2.java
+++ src/SON/MRStep2.java
@@ -1,25 +1,10 @@
 package SON;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-import java.util.Vector;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -28,218 +13,76 @@ import org.apache.hadoop.util.Tool;
 
 public class MRStep2 extends Configured implements Tool {
 
-  private int numReducers;
-  private Path inputPath;
-  private Path outputDir;
+	private int numReducers;
+	private Path inputPath;
+	private Path outputDir;
 
-  @Override
-  public int run(String[] args) throws Exception {
-	  
-	Configuration conf = this.getConf();
-			
-	//define new job instead of null using conf e setting a name
-	Job job = new Job(conf, "SON_step2");
-	//set job input format
-	job.setInputFormatClass(TextInputFormat.class);
-	//set map class and the map output key and value classes
-	job.setMapperClass(MRStep2Mapper.class);
-	job.setMapOutputKeyClass(Itemset.class);
-	job.setMapOutputValueClass(IntWritable.class);
-	//set reduce class and the reduce output key and value classes
-	job.setReducerClass(MRStep2Reducer.class);
-	job.setOutputKeyClass(Itemset.class);
-	job.setOutputValueClass(IntWritable.class);
-	//set job output format
-	job.setOutputFormatClass(TextOutputFormat.class);
-	
-//	//Opzionale:set the combiner class
-//	if(conf.getBoolean("useCombiner", false))
-//		job.setCombinerClass(MRStep2Combiner.class);
-	
-	if(conf.getBoolean("useComparator", true))
-		job.setSortComparatorClass(ItemsetComparator.class);
-	
-	//add the input file as job input (from HDFS)
-	FileInputFormat.addInputPath(job, inputPath);
-	//set the output path for the job results (to HDFS)
-	FileOutputFormat.setOutputPath(job, outputDir);
-	//set the number of reducers. This is optional and by default is 1
-	job.setNumReduceTasks(numReducers);
-	//set the jar class
-	job.setJarByClass(getClass());
-	
-	return job.waitForCompletion(true) ? 0 : 1; // this will execute the job
-  }
+	@Override
+	public int run(String[] args) throws Exception {
 
-  public MRStep2(int numReducers, Path inputPath, Path outputDir){
-	  this.numReducers = numReducers; 
-	  this.inputPath = inputPath;
-	  this.outputDir = outputDir;
-  }
+		Configuration conf = this.getConf();
+
+		// define new job instead of null using conf e setting a name
+		Job job = new Job(conf, "SON_step2");
+		// set job input format
+		job.setInputFormatClass(TextInputFormat.class);
+		// set map class and the map output key and value classes
+		job.setMapperClass(MRStep2Mapper.class);
+		job.setMapOutputKeyClass(Itemset.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		// set reduce class and the reduce output key and value classes
+		job.setReducerClass(MRStep2Reducer.class);
+		job.setOutputKeyClass(Itemset.class);
+		job.setOutputValueClass(IntWritable.class);
+		// set job output format
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// //Opzionale:set the combiner class
+		// if(conf.getBoolean("useCombiner", false))
+		// job.setCombinerClass(MRStep2Combiner.class);
+
+		if (conf.getBoolean("useComparator", true))
+			job.setSortComparatorClass(ItemsetComparator.class);
+
+		// add the input file as job input (from HDFS)
+		FileInputFormat.addInputPath(job, inputPath);
+		// set the output path for the job results (to HDFS)
+		FileOutputFormat.setOutputPath(job, outputDir);
+		// set the number of reducers. This is optional and by default is 1
+		job.setNumReduceTasks(numReducers);
+		// set the jar class
+		job.setJarByClass(getClass());
+
+		return job.waitForCompletion(true) ? 0 : 1; // this will execute the job
+	}
+
+	public MRStep2(int numReducers, Path inputPath, Path outputDir) {
+		this.numReducers = numReducers;
+		this.inputPath = inputPath;
+		this.outputDir = outputDir;
+	}
 
 }
 
-class MRStep2Mapper extends Mapper<LongWritable, 
-									Text,
-									Itemset,
-									IntWritable> {
-
-	HashMap<Vector<Integer>,Integer> candidateItemset = new HashMap<Vector<Integer>,Integer>();
-	
-	
-	@Override
-  	protected void setup(Context context){
-		
-		Path path;
-		FileSystem fs;
-		FSDataInputStream  file;
-		BufferedReader input;
-		FileStatus[] status;
-		String text;
-		
-		try {
-			System.out.println(context.getWorkingDirectory().toString());
-			System.out.println(FileOutputFormat.getOutputPath(context).toString());
-					
-			
-			path = new Path(FileOutputFormat.getOutputPath(context).toString()+"_tmp");
-			fs = FileSystem.get(path.toUri(),context.getConfiguration());
-			status = fs.listStatus(path);
-		
-			for(FileStatus st : status){
-				if(st.isFile() && !st.getPath().toString().contains("_SUCCESS")){
-					//System.out.println("file dir: "+st.getPath());
-					
-					
-					file = fs.open(st.getPath());
-					input = new BufferedReader(new InputStreamReader(file));
-					
-						while ((text = input.readLine()) != null) {
-							text= text.substring(0, text.length()-2).trim();
-							candidateItemset.put(createBasket(text), 0);
-						}
-				}
-			}
-								
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		
-  	}
-	
-	@Override
-  	protected void map( LongWritable key,
-						  Text value,
-						  Context context) throws IOException, InterruptedException {
-		
-		String line = value.toString();
-		Vector<Integer> basket = createBasket(line); 
-		boolean find;
-		
-		for(Vector<Integer> itemset :candidateItemset.keySet()){
-			find = true;
-			for(int item: itemset){
-				if(! basket.contains(item)){
-					find = false;
-					break;
-				}
-			}
-			if(find){
-				candidateItemset.put(itemset, candidateItemset.get(itemset) + 1);
-			}
-		}
-		
-  	}
-	
-
-	public static Vector<Integer> createBasket(String text){
-		StringTokenizer st = null;
-		Vector<Integer> items = new Vector<Integer>();
-		
-		st = new StringTokenizer(text); // Tokenizzo il basket.
-			
-		while(st.hasMoreTokens()) {
-			items.add(Integer.parseInt(st.nextToken())); // Aggiungo ogni item al vettore.
-		}
-		
-		Collections.sort(items);
-		return items;
-	}
-  	
-  
-  	@Override
-  	protected void cleanup(Context context) throws IOException, InterruptedException {
-  		Itemset app = new Itemset();
-  		IntWritable count = new IntWritable();
-  		int temp;
-  		
-  		for(Vector<Integer> itemset :candidateItemset.keySet()){
-  			temp = candidateItemset.get(itemset);
-  			if(temp > 0){
-				app.set(itemset);
-				count.set(temp);
-				context.write(app, count);
-			}
-		}
-  		
-	}
-}
-
-class MRStep2Reducer extends Reducer<Itemset,
-									IntWritable,
-									Itemset,
-									IntWritable> { 
-	
-	double frequent;
-	IntWritable count = new IntWritable();
-	
-	@Override
-  	protected void setup(Context context){
-		long basketReaded = context.getConfiguration().getInt("basketReaded", Integer.MAX_VALUE);
-		double s = context.getConfiguration().getDouble("s", 100);
-		
-		frequent = basketReaded*s/100.0;
-	}
-
-	@Override
-	protected void reduce(Itemset key,
-							Iterable<IntWritable> values, 
-							Context context) throws IOException, InterruptedException {
-		
-		int sum=0;
-		  for(IntWritable i:values)
-			  sum += i.get();
-		  
-		  if(sum >= frequent){
-			  count.set(sum);
-			  context.write(key, count);
-		  }
-		
-	
-	}
-}
-
-
-//class MRStep2Combiner extends Reducer<Itemset,
-//									IntWritable,
-//									Itemset,
-//									IntWritable> { 
-//	
-//	IntWritable count = new IntWritable();	
-//	
-//	@Override
-//	protected void reduce(Itemset key,
-//							Iterable<IntWritable> values, 
-//							Context context) throws IOException, InterruptedException {
-//		
-//		int sum=0;
-//		for(IntWritable i:values)
-//			sum += i.get();
-//		
-//		count.set(sum);
-//		context.write(key, count);
-//	
-//	}
-//}
+// class MRStep2Combiner extends Reducer<Itemset,
+// IntWritable,
+// Itemset,
+// IntWritable> {
+//
+// IntWritable count = new IntWritable();
+//
+// @Override
+// protected void reduce(Itemset key,
+// Iterable<IntWritable> values,
+// Context context) throws IOException, InterruptedException {
+//
+// int sum=0;
+// for(IntWritable i:values)
+// sum += i.get();
+//
+// count.set(sum);
+// context.write(key, count);
+//
+// }
+// }
 
